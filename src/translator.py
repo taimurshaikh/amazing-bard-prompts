f012bd4c4ff8febe8476790f5ee04e2ab21ca247
parkminwoo <parkminwoo1991@gmail.com>
2023-05-15 05:36:06 +0900

    doc: update docstring to translate function

--- src/translator.py
+++ src/translator.py
@@ -1,5 +1,15 @@
-def translate(text, lang):
+def translate(text: str, lang: str):
+    """Returns the text translated through the Google translation engine.
+
+    :param text: text to translate
+    :type text: str
+    :param lang: language to be translated
+    :type lang: str
+    :return: translated text
+    :rtype: _type_
+    """
     from googletrans import Translator
     translator = Translator()
     translated = translator.translate(text, dest=lang)
+    
     return translated.text
